bba853bdbaf697daa031f0c0c29bc7f23c00051e
katerina2018 <43490511+katerina2018@users.noreply.github.com>
2022-11-22 21:56:10 +0000

    Deploying to gh-pages from @ katerina2018/goit-react-hw-08-phonebook@af8355f84a292a43a4c107ec4cbb4ee67500ddcc 🚀

---
+++ static/js/266.b37a2804.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var i=n(184),o={container:{minHeight:"calc(100vh - 80px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsx)("h1",{style:o.title,children:"Contact book"})})}}}]);
+//# sourceMappingURL=266.b37a2804.chunk.js.map
